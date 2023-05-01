85b68148491210f201fc002dd0f44a9f75e136c4
muhiminulalvi <eee1916039brur@gmail.com>
2023-05-01 23:22:41 +0600

    Banner added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React from "react";
 
 const Home = () => {
-    return (
-        <div>
-            This is home
+  return (
+    <div>
+      <div
+        className="hero min-h-screen"
+        style={{
+          backgroundImage: `url("https://images.pexels.com/photos/5773954/pexels-photo-5773954.jpeg")`,
+        }}
+      >
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-lg">
+            <h1 className="mb-5 text-5xl font-bold">M's Cuisine</h1>
+            <p className="mb-5">
+            Korean cuisine is characterized by bold and spicy flavors, fermentation, and an emphasis on vegetables and rice. Korean cuisine is also known for its healthy qualities and emphasis on communal dining.
+            </p>
+            <button className="btn btn-xs btn-warning hover:btn-primary sm:btn-sm md:btn-md lg:btn-lg">See Below</button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Home;
