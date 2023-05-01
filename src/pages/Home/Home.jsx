039cc53d9b453d330abb9ebedd06f21431243c07
muhiminulalvi <eee1916039brur@gmail.com>
2023-05-02 00:12:16 +0600

    about us section added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -20,6 +20,23 @@ const Home = () => {
           </div>
         </div>
       </div>
+      <div className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+        <div className="text-center pt-14 pb-3">
+            <h2 className="font-bold text-4xl py-2">About Us</h2>
+            <p className="font-medium py-3">Korean cuisine is characterized by bold and spicy flavors, fermentation, and an emphasis on vegetables and rice.</p>
+        </div>
+        <div className="grid  md:grid-cols-2 justify-center items-center gap-5 pt-7 pb-14">
+            <div className="mb-3">
+                <h3 className="font-bold text-4xl">Best Cuisine In The City</h3>
+                <p className="font-medium py-3">Korean cuisine is known for its bold and complex flavors, with dishes often featuring a combination of spicy, sour, sweet, and salty tastes. Many dishes are also characterized by their use of fermented ingredients, which add depth and complexity to the flavors.</p>
+                <p className="font-medium py-3">Korean cuisine also features a wide variety of soups, stews, and noodle dishes, often made with a range of meats and vegetables. Seafood is also popular, with dishes like haemul pajeon (seafood pancake) and grilled squid being common favorites.</p>
+                <button className="btn btn-warning font-bold">Know More</button>
+            </div>
+            <div>
+                <img src="/food2.jpg" alt="" />
+            </div>
+        </div>
+      </div>
     </div>
   );
 };
