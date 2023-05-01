594d921980eab06cf4ba17cc787637483c13011f
muhiminulalvi <eee1916039brur@gmail.com>
2023-05-02 01:43:48 +0600

    login registration added

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,11 +1,56 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaGoogle, FaGithub } from 'react-icons/fa';
 
 const Login = () => {
-    return (
-        <div>
-            this is login
-        </div>
-    );
+  return (
+    <div className="px-4 py-14 sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+      <div className="card mx-auto w-full max-w-sm shadow-2xl bg-base-100">
+        <form className="card-body">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              name="email"
+              type="email"
+              placeholder="email"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              name="password"
+              type="password"
+              placeholder="password"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control mt-6">
+            <input type="submit" value="Login" className="btn btn-warning hover:btn font-bold" />
+          </div>
+          <label className="label">
+            <Link to="/register" className="label-text-alt link link-hover font-medium">
+            New to The Korean Cuisine? <span className="text-warning">Create New Account</span>
+            </Link>
+          </label>
+          <hr />
+          <p className="text-center font-bold text-xl">OR</p>
+          <hr />
+
+          <div className="flex flex-col gap-3 pt-3">
+            <button className="btn btn-outline gap-3"><FaGoogle size={20}/> <span>Continue with Google</span></button>
+            <button className="btn btn-outline gap-3"><FaGithub size={20}/> <span>Continue with Github</span></button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,11 +1,83 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaGoogle, FaGithub } from "react-icons/fa";
 
 const Register = () => {
-    return (
-        <div>
-            this is register
-        </div>
-    );
+  return (
+    <div className="px-4 py-14 sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+      <div className="card mx-auto w-full max-w-sm shadow-2xl bg-base-100">
+        <form className="card-body">
+          <h2 className="font-bold text-3xl text-center py-3">
+            Please Register!
+          </h2>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Name</span>
+            </label>
+            <input
+              name="name"
+              type="text"
+              placeholder="name"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              name="email"
+              type="email"
+              placeholder="email"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              name="password"
+              type="password"
+              placeholder="password"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Photo URL</span>
+            </label>
+            <input
+              name="url"
+              type="url"
+              placeholder="photo url"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control mt-6">
+            <input
+              type="submit"
+              value="Register"
+              className="btn btn-warning hover:btn font-bold"
+            />
+          </div>
+          <label className="label">
+            <Link
+              to="/login"
+              className="label-text-alt link link-hover font-medium"
+            >
+              Already have an account? {" "}
+              <span className="text-warning">Login</span>
+            </Link>
+          </label>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
