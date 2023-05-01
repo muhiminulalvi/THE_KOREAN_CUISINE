13e0e9f4354779aa8cae8eff19e4910cf15105c6
muhiminulalvi <eee1916039brur@gmail.com>
2023-05-01 23:06:19 +0600

    footer added

--- src/pages/Footer/Footer.jsx
+++ src/pages/Footer/Footer.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-    return (
+  return (
+    <div className="bg-stone-100">
+      <footer className="footer p-10 bg-base-200 text-base-content px-4 py-10 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
         <div>
-            this is footer
+          <h1 className="text-4xl font-bold">M's Cuisine</h1>
+          <p className="font-medium">
+            M's Cuisine Ltd.
+            <br />
+            Providing reliable recipe since 2002
+          </p>
         </div>
-    );
+        <div>
+          <span className="footer-title">Services</span>
+          <a className="link link-hover">Branding</a>
+          <a className="link link-hover">Design</a>
+          <a className="link link-hover">Marketing</a>
+          <a className="link link-hover">Advertisement</a>
+        </div>
+        <div>
+          <span className="footer-title">Company</span>
+          <a className="link link-hover">About us</a>
+          <a className="link link-hover">Contact</a>
+          <a className="link link-hover">Jobs</a>
+          <a className="link link-hover">Press kit</a>
+        </div>
+        <div>
+          <span className="footer-title">Legal</span>
+          <a className="link link-hover">Terms of use</a>
+          <a className="link link-hover">Privacy policy</a>
+          <a className="link link-hover">Cookie policy</a>
+        </div>
+      </footer>
+    </div>
+  );
 };
 
 export default Footer;
